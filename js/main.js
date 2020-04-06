e76f3ff9c0035e56bf3e575a52dde94355fce9bc
yermakova-l <lyudmyla.yermakova@gmail.com>
2020-04-06 15:13:02 +0300

    Styling languages for mobile menu

--- js/main.js
+++ js/main.js
@@ -59,27 +59,6 @@ $(document).ready(function() {
     }
 })();
 
-/* Slider js */
-$(document).ready(function(){
-    var slideIndex = 0;
-    carousel();
-   
-    function carousel(){
-        
-        var slides_arr = $("div#slider").children("div.slide");
-        
-        for(i = 0; i < slides_arr.length; i++){
-            $(slides_arr[i]).css('display', 'none');
-        }
-        slideIndex++;
-        
-        if (slideIndex > slides_arr.length) {slideIndex = 1}
-        
-        $(slides_arr[slideIndex-1]).css('display', 'block');
-        setTimeout(carousel, 3200);
- 
-    }
-});
 
 /* Scroll reveal js */
  
